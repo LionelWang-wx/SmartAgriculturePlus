62a0408443ccc98f2a4af4c67cf9186c934e7f48
messi1240938550 <1240938550@qq.com>
2020-11-30 14:55:40 +0800

    检测控制页面实现添加设备交互界面优化——王茜

--- app/src/main/java/com/myapp/smartagricultureplus/Monitor_Fragment/Monitor_All_Fragment.java
+++ app/src/main/java/com/myapp/smartagricultureplus/Monitor_Fragment/Monitor_All_Fragment.java
@@ -18,10 +18,10 @@ import java.util.ArrayList;
 public class Monitor_All_Fragment extends Fragment {
     private RecyclerView rcv_monitor_All;
     private ArrayList<Device> devices;
-    int[] deviceIconAll=new int[]{R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,
-            R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,
-            R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,R.mipmap._monitor_img,
-            R.mipmap._monitor_img,R.mipmap._monitor_img};
+    int[] deviceIconAll=new int[]{R.mipmap.img_fan,R.mipmap.img_floorfan,R.mipmap.img_brightnesssensor,R.mipmap.ywcgq,R.mipmap.img_airconditioning,
+            R.mipmap.img_lantern,R.mipmap.img_breaker,R.mipmap.img_temperature,R.mipmap.img_brightnesssensor,R.mipmap.img_floodsensor,R.mipmap._monitor_img,
+            R.mipmap.img_camera,R.mipmap.img_camera,R.mipmap.img_camera,R.mipmap.img_camera,R.mipmap.img_hvac,
+            R.mipmap.img_irrigator,R.mipmap.add_circle};
     String[] deviceNameAll=new String[]{"智能负离子风扇","吸顶式风扇灯","Wi-Fi智能调光器",
     "WiFi水浸报警器","双模智能空调","Wi-Fi智能彩色冷暖光球泡灯","Wi-Fi电量智能统计插座","温度传感器",
     "光照传感器","土壤湿度传感器","风向风力传感器","摄像头传感器","全景摄像头","热成像摄像头","AI摄像头","双模智能电热水器","WiFi门窗磁感应器","更多设备"};
